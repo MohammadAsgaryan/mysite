96a62b7600ccbf0c179a128278accab69f3299d6
MohammadAsgaryan <mohamadasgaryanshad@gmail.com>
2025-07-25 01:30:49 +0330

    set template

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
+from website.views import index_view
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('/index')),
     path('admin/', admin.site.urls),
     path('',include('website.urls')),
 ]
