cdc09fbbb5d461d5621762705402c761bd42e1e3
MohammadAsgaryan <mohamadasgaryanshad@gmail.com>
2025-09-13 16:53:47 +0330

    set db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+    
+    counted_views = models.IntegerField(default=0) 
+    status = models.BooleanField(default=False)
+    published_date = models.DateField(null=True)
+    crreated_at = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+    
+    
+    
