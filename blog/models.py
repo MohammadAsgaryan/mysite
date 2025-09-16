8c13dc150d248c8f12c46784dc4fd229ecdc6257
MohammadAsgaryan <mohamadasgaryanshad@gmail.com>
2025-09-16 10:41:40 +0330

    set blog

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,50 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
-class post(models.Model):
-    title = models.CharField(max_length=250)
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+class Post(models.Model):
+    image = models.ImageField(upload_to='blog/',default='blog/default.jpg')
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    title = models.CharField(max_length=255)
     content = models.TextField()
-    
-    counted_views = models.IntegerField(default=0) 
+    category = models.ManyToManyField(Category)
+    counted_views = models.IntegerField(default=0)
     status = models.BooleanField(default=False)
-    published_date = models.DateField(null=True)
-    crreated_at = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateField(auto_now=True)
+    login_require = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ['-created_date']
+        
+    def __str__(self):
+        return self.title
+
+    def snippets(self):
+        return self.content[:100] + '...'
     
-    
+    def get_absolute_url(self):
+        return reverse('blog:single',kwargs={'pid':self.id})
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.name  
     
