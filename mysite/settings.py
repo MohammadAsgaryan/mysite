d7bf8869d68b5abc6572a8a622725f7d6d76b264
MohammadAsgaryan <mohamadasgaryanshad@gmail.com>
2025-10-20 01:18:03 +0330

    added captcha  and captcha admin

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'multi_captcha_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -92,6 +93,10 @@ SUMMERNOTE_CONFIG = {
     }
 }
 
+# captcha admin settings
+MULTI_CAPTCHA_ADMIN = {
+    'engine': 'simple-captcha',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
